6706351e9bcca635f08a0fbc262294efaaf91027
Freddy Silva <silvahfreddy@gmail.com>
2025-11-19 22:13:02 -0500

    primer commit

---
+++ aws-codepipeline-project/lib/aws-codepipeline-project-stack.ts
@@ -0,0 +1,20 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Construct } from 'constructs';
+
+export class AwsCodepipelineProjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'CDKPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('freddysh/aws-lambda-cicd', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ]
+      })
+    });
+  }
+}
