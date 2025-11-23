bb5a65f7066ff3e6f4a87e55c6d5361d43a398f7
Freddy Silva <silvahfreddy@gmail.com>
2025-11-22 21:53:12 -0500

    update

--- aws-codepipeline-project/lib/aws-codepipeline-project-stack.ts
+++ aws-codepipeline-project/lib/aws-codepipeline-project-stack.ts
@@ -1,5 +1,6 @@
 import { SecretValue, Stack, StackProps } from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
 import { Construct } from 'constructs';
 
 export class AwsCodepipelineProjectStack extends Stack {
@@ -13,16 +14,13 @@ export class AwsCodepipelineProjectStack extends Stack {
           authentication: SecretValue.secretsManager("github-token")
         }),
         commands: [
-          'curl -fsSL https://deb.nodesource.com/setup_20.x | bash -',
-          'apt-get install -y nodejs',
-
-          'node -v',
-          'npm -v',
-
-          'npm install',
+          'npm ci',        // ahora sí funcionará porque npm y node son compatibles
           'npm run build',
           'npx cdk synth'
-        ]
+        ],
+        buildEnvironment: {
+          buildImage: LinuxBuildImage.STANDARD_7_0   // <-- Node 20 + npm 10+
+        }
       })
     });
   }
