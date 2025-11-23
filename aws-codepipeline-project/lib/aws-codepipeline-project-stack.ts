3154b595f7b68e796ca19bf2b96f137e35a73605
Freddy Silva <silvahfreddy@gmail.com>
2025-11-22 21:53:59 -0500

    update

--- aws-codepipeline-project/lib/aws-codepipeline-project-stack.ts
+++ aws-codepipeline-project/lib/aws-codepipeline-project-stack.ts
@@ -1,5 +1,5 @@
 import { SecretValue, Stack, StackProps } from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, CodeBuildStep } from 'aws-cdk-lib/pipelines';
 import { LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
 import { Construct } from 'constructs';
 
@@ -9,17 +9,17 @@ export class AwsCodepipelineProjectStack extends Stack {
 
     new CodePipeline(this, 'Pipeline', {
       pipelineName: 'CDKPipeline',
-      synth: new ShellStep('Synth', {
+      synth: new CodeBuildStep('Synth', {
         input: CodePipelineSource.gitHub('freddysh/aws-lambda-cicd', 'main', {
           authentication: SecretValue.secretsManager("github-token")
         }),
         commands: [
-          'npm ci',        // ahora sí funcionará porque npm y node son compatibles
+          'npm ci',
           'npm run build',
           'npx cdk synth'
         ],
         buildEnvironment: {
-          buildImage: LinuxBuildImage.STANDARD_7_0   // <-- Node 20 + npm 10+
+          buildImage: LinuxBuildImage.STANDARD_7_0   // Node 20 + npm compatible
         }
       })
     });
